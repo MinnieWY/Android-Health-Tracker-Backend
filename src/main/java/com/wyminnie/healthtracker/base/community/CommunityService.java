7339f24fb6cad8791c61b1e9da7555f56debfc65
MinnieWY <minnieeshih@gmail.com>
2024-02-24 22:09:43 +0800

    Bug fix to CommunityService

---
+++ src/main/java/com/wyminnie/healthtracker/base/community/CommunityService.java
@@ -0,0 +1,14 @@
+package com.wyminnie.healthtracker.base.community;
+
+import java.util.Optional;
+
+import com.wyminnie.healthtracker.base.user.User;
+
+public interface CommunityService {
+
+    public boolean addFriendRequest(Optional<User> currentUser, Optional<User> targetUser) throws Exception;
+
+    public boolean isFriend(Optional<User> currentUser, Optional<User> targetUser);
+
+    public QuestionDTO getTodayQuestion();
+}
